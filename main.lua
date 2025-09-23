b8a73c12b22f7760c70745887bf2ed340b473b27
hereiszixiang <2275690280@qq.com>
2025-09-24 00:38:41 +0800

    init

--- main.lua
+++ main.lua
@@ -1,3 +1,22 @@
+
+local num = 0
+
+function love.load()
+    --    image = love.graphics.newImage("cake.jpg")
+    local f = love.graphics.newFont(12)
+    love.graphics.setFont(f)
+    love.graphics.setColor(0, 0, 0, 255)
+    love.graphics.setBackgroundColor(255, 255, 255)
+end
+
+function love.update(dt)
+    if love.keyboard.isDown("up") then
+        num = num + 100 * dt -- this would increment num by 100 per second
+    else
+        num = num - 100 * dt
+    end
+end
+
 function love.draw()
-    love.graphics.print("Hello love",400,300)
+    love.graphics.print("Hello love" .. num, 400, 300)
 end

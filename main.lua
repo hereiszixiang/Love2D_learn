a720d9742ddc8b52667e3be7fc9327ff4cafef67
hereiszixiang <2275690280@qq.com>
2025-10-13 01:06:46 +0800

    多文件,局部变量,表对象

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
-
+require("example")
 local num = 0
+print(7)
 
 function love.load()
     --    image = love.graphics.newImage("cake.jpg")
@@ -7,6 +8,8 @@ function love.load()
     love.graphics.setFont(f)
     love.graphics.setColor(0, 0, 0, 255)
     love.graphics.setBackgroundColor(255, 255, 255)
+
+    listOfRectangles = {}
 end
 
 function love.update(dt)
@@ -15,10 +18,36 @@ function love.update(dt)
     else
         num = num - 100 * dt
     end
+    print("tttt")
+    for index, value in ipairs(listOfRectangles) do
+        value.x = value.x + value.speed * dt
+    end
 end
 
 function love.draw()
     love.graphics.print("Hello love" .. num, 400, 300)
+    for index, value in ipairs(listOfRectangles) do
+        love.graphics.rectangle("line",value.x,value.y,value.width,value.height)
+    end
 end
 
+
+
+function createRect()
+    rect = {}
+    rect.x = 100
+    rect.y = 100
+    rect.width = 70
+    rect.height = 90
+    rect.speed = 100
+
+    -- Put the new rectangle in the list
+    table.insert(listOfRectangles, rect)
+end
+
+function love.keypressed(key)
+    if key == "space" then
+        createRect()
+    end
+end
 -- ttt
